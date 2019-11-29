c73ce291a7e9940cca5858ae730114a8f23813b7
AxelBrinck <axel.louis.1991@gmail.com>
2019-11-29 21:58:47 +0100

    Started creation of a driver that will abstract functionality of a two dimensional node cursor.

---
+++ NodeDbBasicDriver.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+namespace NodeMaps
+{
+    public class NodeDbBasicDriver
+    {
+        private readonly INodeMap2D _nodeMap2D;
+        
+        public NodeDbBasicDriver(Stream stream)
+        {
+            _nodeMap2D = new StreamNodeMap2D(stream);
+        }
+    }
+}
