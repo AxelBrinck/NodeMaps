c5a96915eeb99b9693e96042325ee6f8ea7b9546
AxelBrinck <axel.louis.1991@gmail.com>
2019-12-03 10:50:40 +0100

    Created a basic binary stream node interpreter

---
+++ Interpreters/INodeMapInterpreter.cs
@@ -0,0 +1,16 @@
+﻿using NodeMaps.Entities;
+
+namespace NodeMaps.Interpreters
+{
+    public interface INodeMapInterpreter
+    {
+        long Address { get; set; }
+        Node CurrentNode { get; set; }
+        Node LeftNode { get; set; }
+        Node RightNode { get; set; }
+        Node UpNode { get; set; }
+        Node DownNode { get; set; }
+        Node FrontNode { get; set; }
+        Node BackNode { get; set; }
+    }
+}
