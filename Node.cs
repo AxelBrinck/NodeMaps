6613b1fed4defd89bf356f19b879ea23636cfe0a
AxelBrinck <axel.louis.1991@gmail.com>
2019-12-03 09:51:07 +0100

    Added a node basic entity.

---
+++ Node.cs
@@ -0,0 +1,29 @@
+﻿namespace NodeMaps
+{
+    /// <summary>
+    /// Describes a single node with all its attributes.
+    /// </summary>
+    public class Node
+    {
+        public long Address { get; }
+        public long Data { get; }
+        public long Left { get; }
+        public long Right { get; }
+        public long Up { get; }
+        public long Down { get; }
+        public long Front { get; }
+        public long Back { get; }
+
+        public Node(long address, long data, long left, long right, long up, long down, long front, long back)
+        {
+            Address = address;
+            Data = data;
+            Left = left;
+            Right = right;
+            Up = up;
+            Down = down;
+            Front = front;
+            Back = back;
+        }
+    }
+}
