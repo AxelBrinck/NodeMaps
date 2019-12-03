ba69a1e11ef89c4acef3f8594803c5dc5b00325c
AxelBrinck <axel.louis.1991@gmail.com>
2019-12-03 11:34:31 +0100

    Abstracted Stream format from actual implementation. You can now code different implementations by extending the StreamNodeMapFormat class.

--- Formatting/Stream/BinaryStreamNodeMapFormat.cs
+++ Formatting/Stream/BinaryStreamNodeMapFormat.cs
@@ -3,116 +3,54 @@ using NodeMaps.Entities;
 
 namespace NodeMaps.Formatting.Stream
 {
-    public class BinaryStreamNodeMapFormat : INodeMapFormat<byte[]>
+    public class BinaryStreamNodeMapFormat : StreamNodeMapFormat<byte[]>
     {
-        private readonly System.IO.Stream _stream;
-        private readonly BinaryReader _reader;
-        private readonly BinaryWriter _writer;
+        public BinaryStreamNodeMapFormat(System.IO.Stream stream) : base(stream) {}
 
-        private long _currentAddress;
-        private Node _currentNode;
-        
-        public BinaryStreamNodeMapFormat(System.IO.Stream stream)
-        {
-            _stream = stream;
-            _reader = new BinaryReader(_stream);
-            _writer = new BinaryWriter(_stream);
-        }
-
-        public long Address
-        {
-            get => _currentAddress;
-            set
-            {
-                _currentNode = null;
-                _currentAddress = value;
-            }
-        }
-
-        public Node CurrentNode
-        {
-            get => _currentNode ??= ReadNodeFromAddress(Address);
-            set => WriteNodeToAddress(Address, value);
-        }
-
-        public Node LeftNode
-        {
-            get => ReadNodeFromAddress(CurrentNode.Left);
-            set => WriteNodeToAddress(CurrentNode.Left, value);
-        }
-        
-        public Node RightNode
-        {
-            get => ReadNodeFromAddress(CurrentNode.Right);
-            set => WriteNodeToAddress(CurrentNode.Right, value);
-        }
-        
-        public Node UpNode
-        {
-            get => ReadNodeFromAddress(CurrentNode.Up);
-            set => WriteNodeToAddress(CurrentNode.Up, value);
-        }
-        
-        public Node DownNode
-        {
-            get => ReadNodeFromAddress(CurrentNode.Down);
-            set => WriteNodeToAddress(CurrentNode.Down, value);
-        }
-        
-        public Node FrontNode
-        {
-            get => ReadNodeFromAddress(CurrentNode.Front);
-            set => WriteNodeToAddress(CurrentNode.Front, value);
-        }
-        
-        public Node BackNode
-        {
-            get => ReadNodeFromAddress(CurrentNode.Back);
-            set => WriteNodeToAddress(CurrentNode.Back, value);
-        }
-
-        public byte[] Data
+        public override byte[] Data
         {
             get
             {
-                _stream.Position = CurrentNode.Data;
-                var length = _reader.ReadInt16();
-                return _reader.ReadBytes(length);
+                Stream.Position = CurrentNode.Data;
+                var length = Reader.ReadInt16();
+                return Reader.ReadBytes(length);
             }
             set
             {
-                _stream.Position = CurrentNode.Data;
-                _writer.Write((short) value.Length);
-                _writer.Write(value);
-                _writer.Flush();
+                Stream.Position = CurrentNode.Data;
+                Writer.Write((short) value.Length);
+                Writer.Write(value);
+                Writer.Flush();
             }
         }
 
-        private Node ReadNodeFromAddress(long address)
+        protected override Node ReadNodeFromAddress(long address)
         {
-            _stream.Position = address;
+            Stream.Position = address;
             return new Node(
                 address,
-                _reader.ReadInt64(),
-                _reader.ReadInt64(),
-                _reader.ReadInt64(),
-                _reader.ReadInt64(),
-                _reader.ReadInt64(),
-                _reader.ReadInt64(),
-                _reader.ReadInt64());
+                Reader.ReadInt64(),
+                Reader.ReadInt64(),
+                Reader.ReadInt64(),
+                Reader.ReadInt64(),
+                Reader.ReadInt64(),
+                Reader.ReadInt64(),
+                Reader.ReadInt64());
         }
 
-        private void WriteNodeToAddress(long address, Node node)
+        protected override void WriteNodeToAddress(long address, Node node)
         {
-            _stream.Position = address;
-            _writer.Write(node.Data);
-            _writer.Write(node.Left);
-            _writer.Write(node.Right);
-            _writer.Write(node.Up);
-            _writer.Write(node.Down);
-            _writer.Write(node.Front);
-            _writer.Write(node.Back);
-            _writer.Flush();
+            Stream.Position = address;
+            Writer.Write(node.Data);
+            Writer.Write(node.Left);
+            Writer.Write(node.Right);
+            Writer.Write(node.Up);
+            Writer.Write(node.Down);
+            Writer.Write(node.Front);
+            Writer.Write(node.Back);
+            Writer.Flush();
         }
+
+        
     }
 }
--- Formatting/Stream/StreamNodeMapFormat.cs
+++ Formatting/Stream/StreamNodeMapFormat.cs
@@ -1,7 +1,79 @@
-﻿namespace NodeMaps.Formatting.Stream
+﻿using System.IO;
+using NodeMaps.Entities;
+
+namespace NodeMaps.Formatting.Stream
 {
-    public abstract class StreamNodeMapFormat
+    public abstract class StreamNodeMapFormat<T>
     {
+        protected readonly System.IO.Stream Stream;
+        protected readonly BinaryReader Reader;
+        protected readonly BinaryWriter Writer;
+
+        private long _currentAddress;
+        private Node _currentNode;
         
+        public StreamNodeMapFormat(System.IO.Stream stream)
+        {
+            Stream = stream;
+            Reader = new BinaryReader(Stream);
+            Writer = new BinaryWriter(Stream);
+        }
+        
+        public long Address
+        {
+            get => _currentAddress;
+            set
+            {
+                _currentNode = null;
+                _currentAddress = value;
+            }
+        }
+
+        public Node CurrentNode
+        {
+            get => _currentNode ??= ReadNodeFromAddress(Address);
+            set => WriteNodeToAddress(Address, value);
+        }
+
+        public Node LeftNode
+        {
+            get => ReadNodeFromAddress(CurrentNode.Left);
+            set => WriteNodeToAddress(CurrentNode.Left, value);
+        }
+        
+        public Node RightNode
+        {
+            get => ReadNodeFromAddress(CurrentNode.Right);
+            set => WriteNodeToAddress(CurrentNode.Right, value);
+        }
+        
+        public Node UpNode
+        {
+            get => ReadNodeFromAddress(CurrentNode.Up);
+            set => WriteNodeToAddress(CurrentNode.Up, value);
+        }
+        
+        public Node DownNode
+        {
+            get => ReadNodeFromAddress(CurrentNode.Down);
+            set => WriteNodeToAddress(CurrentNode.Down, value);
+        }
+        
+        public Node FrontNode
+        {
+            get => ReadNodeFromAddress(CurrentNode.Front);
+            set => WriteNodeToAddress(CurrentNode.Front, value);
+        }
+        
+        public Node BackNode
+        {
+            get => ReadNodeFromAddress(CurrentNode.Back);
+            set => WriteNodeToAddress(CurrentNode.Back, value);
+        }
+        
+        public abstract T Data { get; set; }
+
+        protected abstract Node ReadNodeFromAddress(long address);
+        protected abstract void WriteNodeToAddress(long address, Node node);
     }
 }
