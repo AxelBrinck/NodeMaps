6e5dac7d9b08f39ba50ec2fd1a709694d544198e
AxelBrinck <axel.louis.1991@gmail.com>
2019-12-03 13:59:20 +0100

    Initializing to root node.

--- Drivers/NodeMapDriver.cs
+++ Drivers/NodeMapDriver.cs
@@ -6,10 +6,12 @@ namespace NodeMaps.Drivers
     public class NodeMapDriver<T>
     {
         private readonly INodeMapFormat<T> _format;
-        
+
         public NodeMapDriver(INodeMapFormat<T> format)
         {
             _format = format;
+            _format.Id = 0;
+            CurrentNode = _format.CurrentNode;
         }
         
         public Node CurrentNode { get; private set; }
