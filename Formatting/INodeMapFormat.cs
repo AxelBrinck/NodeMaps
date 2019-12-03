c3aa25543d71ea33923e969cc4ad4da2c3fe4c60
AxelBrinck <axel.louis.1991@gmail.com>
2019-12-03 18:00:27 +0100

    Project ready to integrate a more versatile code.

--- Formatting/INodeMapFormat.cs
+++ Formatting/INodeMapFormat.cs
@@ -5,13 +5,6 @@ namespace NodeMaps.Formatting
     public interface INodeMapFormat<T>
     {
         long Id { get; set; }
-        Node CurrentNode { get; set; }
-        long LeftNodeId { get; set; }
-        long RightNodeId { get; set; }
-        long UpNodeId { get; set; }
-        long DownNodeId { get; set; }
-        long FrontNodeId { get; set; }
-        long BackNodeId { get; set; }
         T Data { get; set; }
         long GetEmptyId();
     }
--- Formatting/Stream/StreamNodeMapFormat.cs
+++ Formatting/Stream/StreamNodeMapFormat.cs
@@ -17,74 +17,7 @@ namespace NodeMaps.Formatting.Stream
         }
         
         public long Id { get; set; }
-
-
-        public Node CurrentNode
-        {
-            get => ReadNodeFromAddress(Id);
-            set => WriteNodeToAddress(Id, value);
-        }
-
-        public long LeftNodeId
-        {
-            get => ReadNodeFromAddress(Id).LeftId;
-            set
-            {
-                Stream.Position = Id;
-                SetLeftNodeId(value);
-            }
-        }
-
-        public long RightNodeId
-        {
-            get => ReadNodeFromAddress(Id).RightId;
-            set
-            {
-                Stream.Position = Id;
-                SetRightNodeId(value);
-            }
-        }
-
-        public long UpNodeId
-        {
-            get => ReadNodeFromAddress(Id).UpId;
-            set
-            {
-                Stream.Position = Id;
-                SetUpNodeId(value);
-            }
-        }
-
-        public long DownNodeId
-        {
-            get => ReadNodeFromAddress(Id).DownId;
-            set
-            {
-                Stream.Position = Id;
-                SetDownNodeId(value);
-            }
-        }
-
-        public long FrontNodeId
-        {
-            get => ReadNodeFromAddress(Id).FrontId;
-            set
-            {
-                Stream.Position = Id;
-                SetFrontNodeId(value);
-            }
-        }
-
-        public long BackNodeId
-        {
-            get => ReadNodeFromAddress(Id).BackId;
-            set
-            {
-                Stream.Position = Id;
-                SetBackNodeId(value);
-            }
-        }
-
+        
         public long GetEmptyId()
         {
             return Stream.Length;
@@ -92,13 +25,5 @@ namespace NodeMaps.Formatting.Stream
         
         public abstract T Data { get; set; }
 
-        protected abstract Node ReadNodeFromAddress(long address);
-        protected abstract void WriteNodeToAddress(long address, Node node);
-        protected abstract void SetLeftNodeId(long id);
-        protected abstract void SetRightNodeId(long id);
-        protected abstract void SetUpNodeId(long id);
-        protected abstract void SetDownNodeId(long id);
-        protected abstract void SetFrontNodeId(long id);
-        protected abstract void SetBackNodeId(long id);
     }
 }
