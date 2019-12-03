5a576d70275107070a64f672a887719bcec5e3e7
AxelBrinck <axel.louis.1991@gmail.com>
2019-12-03 20:55:31 +0100

    Reading bytes at target location.

---
+++ Formatting/Stream/BinaryStreamNodeMapFormat.cs
@@ -0,0 +1,40 @@
+﻿using NodeMaps.Entities;
+
+namespace NodeMaps.Formatting.Stream
+{
+    public class BinaryStreamNodeMapFormat : StreamNodeMapFormat<byte[]>
+    {
+        public BinaryStreamNodeMapFormat(System.IO.Stream stream) : base(stream) { }
+
+        public override void GotoId(long id) => Stream.Position = id;
+
+        public override byte[] GetData()
+        {
+            Stream.Position = CurrentId;
+            Stream.Position += sizeof(long);
+            Stream.Position = Reader.ReadInt64();
+            return Reader.ReadBytes(Reader.ReadInt16());
+        }
+
+        public override void SetData(byte[] data)
+        {
+            Stream.Position = CurrentId;
+            
+            throw new System.NotImplementedException();
+        }
+
+        public override long GetTargetNodeId(Direction direction)
+        {
+            Stream.Position = CurrentId;
+            
+            throw new System.NotImplementedException();
+        }
+
+        public override void SetTargetNodeId(Direction direction, long nodeId)
+        {
+            Stream.Position = CurrentId;
+            
+            throw new System.NotImplementedException();
+        }
+    }
+}
