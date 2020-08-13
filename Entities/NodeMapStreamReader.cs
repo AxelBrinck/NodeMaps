b795b58c53002d61812a807699536396dad3d8d7
Axel <axel.louis.1991@gmail.com>
2020-08-13 19:01:34 +0200

    Created base stream entities

---
+++ Entities/NodeMapStreamReader.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+namespace NodeMaps.Entities
+{
+    public class NodeMapStreamReader : NodeMapStreamBase
+    {
+        protected readonly BinaryReader StreamReader;
+        
+        public NodeMapStreamReader(Stream stream) : base(stream)
+        {
+            StreamReader = new BinaryReader(stream);
+        }
+    }
+}
---
+++ Entities/NodeMapStreamReaderWriter.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+namespace NodeMaps.Entities
+{
+    public class NodeMapStreamReaderWriter : NodeMapStreamReader
+    {
+        protected readonly StreamWriter StreamWriter;
+        
+        public NodeMapStreamReaderWriter(Stream stream) : base(stream)
+        {
+            StreamWriter = new StreamWriter(stream);
+        }
+    }
+}
