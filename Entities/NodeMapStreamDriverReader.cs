a4ef1e7d023d97785054d9996bc7f243a821a48d
Axel <axel.louis.1991@gmail.com>
2020-08-13 20:11:04 +0200

    Basic reading operations

--- Entities/NodeMapStreamDriverReader.cs
+++ Entities/NodeMapStreamDriverReader.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 
 namespace NodeMaps.Entities
 {
@@ -7,9 +6,48 @@ namespace NodeMaps.Entities
     {
         private readonly BinaryReader _streamReader;
 
+        public long HeaderAddress => _streamReader.BaseStream.Position;
+
         public NodeMapStreamReader(Stream streamBase)
         {
             _streamReader = new BinaryReader(streamBase);
+        }
+
+        public long GetDataAddress()
+        {
+            var headerAddress = HeaderAddress;
+            var dataAddress = _streamReader.ReadInt64();
+            _streamReader.BaseStream.Position = headerAddress;
+            return dataAddress;
+        }
+
+        public int GetReferenceCount()
+        {
+            var headerAddress = HeaderAddress;
+            _streamReader.BaseStream.Seek(sizeof(long), SeekOrigin.Current);
+            var referenceCount = _streamReader.ReadInt32();
+            _streamReader.BaseStream.Position = headerAddress;
+            return referenceCount;
+        }
+
+        public long GetReferenceAddress(int slot)
+        {
+            var headerAddress = HeaderAddress;
+            _streamReader.BaseStream.Seek(sizeof(long) + sizeof(int) * slot, SeekOrigin.Current);
+            var referenceAddress = _streamReader.ReadInt64();
+            _streamReader.BaseStream.Position = headerAddress;
+            return referenceAddress;
+        }
+
+        public byte[] GetData()
+        {
+            var headerAddress = HeaderAddress;
+            var dataAddress = _streamReader.ReadInt64();
+            _streamReader.BaseStream.Position = dataAddress;
+            var dataLength = _streamReader.ReadInt32();
+            var data = _streamReader.ReadBytes(dataLength);
+            _streamReader.BaseStream.Position = headerAddress;
+            return data;
         }
     }
 }
