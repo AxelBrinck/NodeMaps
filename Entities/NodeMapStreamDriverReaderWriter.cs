5dce44d5042a317804ff577af80cbbad74a49dff
Axel <axel.louis.1991@gmail.com>
2020-08-14 02:36:14 +0200

    node writer can write headers

--- Entities/NodeMapStreamDriverReaderWriter.cs
+++ Entities/NodeMapStreamDriverReaderWriter.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace NodeMaps.Entities
 {
@@ -9,6 +10,32 @@ namespace NodeMaps.Entities
         public NodeMapStreamReaderWriter(Stream streamBase) : base(streamBase)
         {
             _streamWriter = new StreamWriter(streamBase);
+        }
+
+        public void Initialize(int referenceCount)
+        {
+            if (_streamWriter.BaseStream.Length > 0) 
+                throw new Exception("Cannot initialize a non-empty stream.");
+
+            WriteEmptyHeader(new NodeMapReferenceAddress(new NodeMapHeaderAddress(0), 0), referenceCount);
+        }
+
+        public void Create(NodeMapReferenceAddress address, int referenceCount)
+        {
+            WriteEmptyHeader(address, referenceCount);
+        }
+
+        private void WriteEmptyHeader(NodeMapReferenceAddress address, int referenceCount)
+        {
+            _streamWriter.BaseStream.Position = address.ReferenceStreamPosition;
+            
+            _streamWriter.Write((long) -1);
+            _streamWriter.Write(referenceCount);
+            
+            for (var i = 0; i < referenceCount; i++)
+                _streamWriter.Write((long) -1);
+
+            _streamWriter.BaseStream.Position = 0;
         }
     }
 }
