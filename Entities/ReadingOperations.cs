0634e2b0339a75d3ebec44d0c379bd94666235fc
Axel <axel.louis.1991@gmail.com>
2020-08-16 15:22:30 +0200

    New classes that has all reading and writing operations

---
+++ Entities/ReadingOperations.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using NodeMaps.Entities.Data;
+
+namespace NodeMaps.Entities
+{
+    public class ReadingOperations
+    {
+        private readonly BlockReader _reader;
+
+        public ReadingOperations(Stream stream)
+        {
+            _reader = new BlockReader(stream);
+        }
+
+        public DataBlock ReadDataBlock(long position)
+        {
+            return _reader.ReadBlock<DataBlock>(position);
+        }
+
+        public GateBlock ReadGateBlock(long position)
+        {
+            return _reader.ReadBlock<GateBlock>(position);
+        }
+    }
+}
---
+++ Entities/WritingOperations.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using NodeMaps.Entities.Data;
+
+namespace NodeMaps.Entities
+{
+    public class WritingOperations
+    {
+        private readonly BlockWriter _writer;
+
+        public WritingOperations(Stream stream)
+        {
+            _writer = new BlockWriter(stream);
+        }
+
+        public void WriteDataBlock(long position, DataBlock dataBlock)
+        {
+            _writer.WriteBlock(position, dataBlock);
+        }
+
+        public void WriteGateBlock(long position, GateBlock gateBlock)
+        {
+            _writer.WriteBlock(position, gateBlock);
+        }
+    }
+}
