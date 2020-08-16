039c72ca668cc0478dc7aa1b5a675a04d9927a0e
Axel <axel.louis.1991@gmail.com>
2020-08-16 15:30:02 +0200

    Comments added.

--- Entities/ReadingOperations.cs
+++ Entities/ReadingOperations.cs
@@ -3,20 +3,38 @@ using NodeMaps.Entities.Data;
 
 namespace NodeMaps.Entities
 {
+    
+    /// <summary>
+    /// Encapsulates all the reading operations.
+    /// </summary>
     public class ReadingOperations
     {
         private readonly BlockReader _reader;
 
+        /// <summary>
+        /// Instantiate by providing a stream to read.
+        /// </summary>
+        /// <param name="stream">The stream to read.</param>
         public ReadingOperations(Stream stream)
         {
             _reader = new BlockReader(stream);
         }
 
+        /// <summary>
+        /// Reads a data block from the specified stream position.
+        /// </summary>
+        /// <param name="position">The stream position to read the data block.</param>
+        /// <returns>The gathered data block from the stream.</returns>
         public DataBlock ReadDataBlock(long position)
         {
             return _reader.ReadBlock<DataBlock>(position);
         }
 
+        /// <summary>
+        /// Reads a gate block from the specified stream position.
+        /// </summary>
+        /// <param name="position">The stream position to read the gate block.</param>
+        /// <returns>The gathered gate block from the stream.</returns>
         public GateBlock ReadGateBlock(long position)
         {
             return _reader.ReadBlock<GateBlock>(position);
--- Entities/WritingOperations.cs
+++ Entities/WritingOperations.cs
@@ -3,20 +3,38 @@ using NodeMaps.Entities.Data;
 
 namespace NodeMaps.Entities
 {
+    
+    /// <summary>
+    /// Encapsulates all the writing operations.
+    /// </summary>
     public class WritingOperations
     {
         private readonly BlockWriter _writer;
 
+        /// <summary>
+        /// Instantiate by providing a stream to write.
+        /// </summary>
+        /// <param name="stream">The stream to write.</param>
         public WritingOperations(Stream stream)
         {
             _writer = new BlockWriter(stream);
         }
 
+        /// <summary>
+        /// Writes a data block to the specified stream position.
+        /// </summary>
+        /// <param name="position">The stream position to write the data block.</param>
+        /// <param name="dataBlock">The data block to write to the stream.</param>
         public void WriteDataBlock(long position, DataBlock dataBlock)
         {
             _writer.WriteBlock(position, dataBlock);
         }
 
+        /// <summary>
+        /// Writes a gate block to the specified stream position.
+        /// </summary>
+        /// <param name="position">The stream position to write the gate block.</param>
+        /// <param name="gateBlock">The gate block to write to the stream.</param>
         public void WriteGateBlock(long position, GateBlock gateBlock)
         {
             _writer.WriteBlock(position, gateBlock);
