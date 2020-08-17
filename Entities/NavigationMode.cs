754215bfdf6e72847666f073b7d09fc59bd12777
Axel <axel.louis.1991@gmail.com>
2020-08-17 18:26:31 +0200

    defining node operations

---
+++ Entities/NavigationMode.cs
@@ -0,0 +1,18 @@
+﻿namespace NodeMaps.Entities
+{
+    /// <summary>
+    /// This will set a navigation mode for traversing nodes with the <see cref="NodeOperations"/> object.
+    /// </summary>
+    public enum NavigationMode
+    {
+        /// <summary>
+        /// Will only enable reading without any modification capability.
+        /// </summary>
+        Read,
+        
+        /// <summary>
+        /// Will be able to read and write nodes.
+        /// </summary>
+        ReadWrite
+    }
+}
